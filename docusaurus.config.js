37b21692a674a5f26e950a8c1c2516a075c48dd7
Мария Александровна <ml@webpractik.ru>
2022-05-13 23:58:11 +0300

    feat: добавлены документы, статья о ключах, провайдера, мелкие корректировки

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/north-leshiy/tale',
             label: 'GitHub',
             position: 'right',
           },
