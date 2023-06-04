f0485a2eceb09517d343324f2f23f8fb1beca8e6
Иван Поддубный <ip@webpractik.ru>
2023-06-04 21:17:34 +0300

    fix(config): fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,82 +1,87 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ЖК сказка',
-  tagline: 'База знаний чата жильцов',
-  url: 'https://north-leshiy.github.io',
-  baseUrl: '/tale/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'north-leshiy', // Usually your GitHub org/user name.
-  projectName: 'tale', // Usually your repo name.
+  title: "ЖК сказка",
+  tagline: "База знаний чата жильцов",
+  url: "https://north-leshiy.github.io",
+  baseUrl: "/tale/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "north-leshiy", // Usually your GitHub org/user name.
+  projectName: "tale", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: "https://github.com/north-leshiy/tale/edit/master",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
-  plugins: [[ require.resolve('docusaurus-lunr-search'), {
-    languages: ['en', 'ru'] // language codes
-  }]],
+  plugins: [
+    [
+      require.resolve("docusaurus-lunr-search"),
+      {
+        languages: ["en", "ru"], // language codes
+      },
+    ],
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'База знаний',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "База знаний",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/north-leshiy/tale',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/north-leshiy/tale",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
             items: [
               {
-                label: 'Главный чат дома',
-                to: 'https://t.me/+_X_hHgL30RQxM2My',
+                label: "Главный чат дома",
+                to: "https://t.me/+_X_hHgL30RQxM2My",
               },
             ],
           },
           {
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
